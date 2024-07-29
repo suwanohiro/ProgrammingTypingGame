892031915f2474f161d1437ab67a291bc2f6a8ba
suwanohiro <supermariomaker1110@gmail.com>
2024-07-29 13:32:28 +0900

    コンソールで入力判定を確認

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,6 +5,27 @@ export default class MainApplication {
     }
 
     public start(): void {
-        alert("Hello World");
+        console.log(this.targetString);
+        window.addEventListener("keydown", (event: KeyboardEvent) => {
+            this.__keydownEvent(event.key);
+        });
+    }
+
+    private targetString: string = ("Hello, World!").toLowerCase();
+    private targetIndex: number = 0;
+
+    private ignoreIndex: Array<string> = [
+        "Shift", "Control", "Alt", "Meta", "ArrowLeft", "ArrowRight", "Backspace", "Delete", "Enter", "Tab", "Escape"
+    ];
+
+    private __keydownEvent(key: string): void {
+        if (this.ignoreIndex.indexOf(key) >= 0) return;
+
+        if (key === this.targetString[this.targetIndex]) {
+            this.targetIndex++;
+            console.log("correct");
+        } else {
+            console.error(`miss\tkey: ${key}`);
+        }
     }
 }
