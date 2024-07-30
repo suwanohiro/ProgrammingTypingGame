5f2e38204dbd806aa707300adaaf2a7ffe75e865
suwanohiro <supermariomaker1110@gmail.com>
2024-07-30 19:52:41 +0900

    ゲーム開始時にカウントダウンする処理を実装

--- src/ts/Game/main.ts
+++ src/ts/Game/main.ts
@@ -6,7 +6,28 @@ export default class MainApplication {
         app.start();
     }
 
+    private _waitTime: number = 3;
+
     public start(): void {
-        Game.main();
+        this.__countDown();
+    }
+
+    private __countDown(): void {
+        const countDown: HTMLDivElement = document.getElementById("countDown")! as HTMLDivElement;
+        countDown.style.display = "block";
+        setTimeout(() => {
+            let renderString: string = this._waitTime.toString();
+            if (this._waitTime < 1) renderString = "GO!";
+            countDown.innerHTML = renderString;
+            if (this._waitTime < 0) {
+                countDown.style.display = "none";
+
+                // ゲームスタート
+                setTimeout(() => { Game.main(); }, 250);
+                return;
+            }
+            this._waitTime--;
+            this.__countDown();
+        }, 1000);
     }
 }
